c56a66300c3627887de827ec398106df385da152
Patrick Heusinger <92826589+PatrickHeusinger@users.noreply.github.com>
2022-07-15 13:52:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+hours = input('Enter your money per hour : ')
+day = 8 * int(hours)
+week = day * 5
+month = week * 4
+year = month * 12
+
+print('Money per hour : ' + str(hours) + ' $')
+print('Money per day ' + str(day) + ' $')
+print('Money per week : ' + str(week) + ' $')
+print('Money per month : ' + str(month) + ' $')
+print('Money per year :' + str(year) + ' $')
